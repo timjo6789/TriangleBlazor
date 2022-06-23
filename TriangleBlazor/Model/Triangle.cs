ab866ead98b9a31b281b99ab5d698925b5663300
Timothy Johnson <tgj2u2@gmail.com>
2022-06-22 21:54:09 -0600

    adding Triangle and validation for sides.

---
+++ TriangleBlazor/Model/Triangle.cs
@@ -0,0 +1,33 @@
+﻿namespace TriangleBlazor.Model
+{
+    public class Triangle
+    {
+        public int SideA;
+        public int SideB;
+        public int SideC;
+
+        public Triangle(int sideA = 0, int sideB = 0, int sideC = 0)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+        public bool IsValid()
+        {
+            // verify combinations of each side
+            bool AB_C = ValidateSides(SideA, SideB, SideC);
+            bool AC_B = ValidateSides(SideC, SideA, SideB);
+            bool BC_A = ValidateSides(SideB, SideC, SideA);
+
+            // only return true if all sides are validated
+            return AB_C && AC_B && BC_A;
+        }
+
+        //public string ClassifyTriangle()
+        //{
+        //    return "";
+        //}
+
+        private bool ValidateSides(int a, int b, int c) => a + b > c;
+    }
+}
